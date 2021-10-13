c07c3bdbbe49ab8f87ddcd00fcb8ef4afde76bd6
naveen kala <34640475+kalanaveen@users.noreply.github.com>
2021-10-13 12:39:46 +0530

    Update covidApi.js

--- src/services/covidApi.js
+++ src/services/covidApi.js
@@ -2,7 +2,7 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const covidApiHeaders = {
     'x-rapidapi-host': 'covid-19-v1.p.rapidapi.com',
-    'x-rapidapi-key': 'UogVWI5hRimshntTCs93EGVJEZiup1jtXgGjsnaUsU08pcCQGR'
+    'x-rapidapi-key': ''
 }
 
 const baseUrl = "https://covid-19-v1.p.rapidapi.com";
